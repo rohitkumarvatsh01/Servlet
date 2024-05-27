551291c09f55457832a33fc51b722b32962df9f0
rohitkumarvatsh01 <rohitkumarvatsh01@gmail.com>
2024-05-28 00:38:13 +0530

    First Commit

---
+++ src/main/java/com/servlet/GenericServletClass.java
@@ -0,0 +1,23 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class GenericServletClass extends GenericServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="WebServlet" method="get">
+		<label id="UserName">Name: </label>
+		<input type="text" name="UserName" id="UserName">
+		
+		<label id="Password">Password: </label>
+		<input type="text" name="Password" id="Password">
+		
+		<button>Submit</button>
+	</form>
+</body>
+</html>
